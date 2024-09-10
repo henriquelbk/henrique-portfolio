b298c98ba57f991db1e1859a1b600c0093df824d
henriquelbk <loubackhenrique123@gmail.com>
2024-09-09 21:48:15 -0300

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], 
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"], variable: "--font-jetbrainsMono",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable}>
+        <Header />
         {children}
       </body>
     </html>
