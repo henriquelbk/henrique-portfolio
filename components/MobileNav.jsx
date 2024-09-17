98d489e70d5763ec2c494b404149ab5ac8d809f4
henriquelbk <loubackhenrique123@gmail.com>
2024-09-16 21:40:15 -0300

    mobile nav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,8 +1,42 @@
+"use client";
 
+import { Sheet, SheetContent, SheetTrigger} from "@/components/ubutton/buttoni/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
 
 const MobileNav = () => {
+  const path = usePathname();
   return (
-    <div>MobileNav</div>
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent>Content</SheetContent>
+    </Sheet>
   )
 }
 
