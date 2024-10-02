54dfe70e98db1e9ed7e250dfe4c407a1d7dc8fce
henriquelbk <loubackhenrique123@gmail.com>
2024-10-01 22:23:29 -0300

    Socials imported

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Social from "@/components/Social";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -15,11 +16,14 @@ const Home = () => {
             <p className="max-x-[500px] mb-9 text-white/80">I excel at crafting usefull software and I am proficient in various programming technologies.
             </p>
             {/* btn and socials*/}
-            <div>
+            <div className="flex flex-col xl: flex-row items-center gap-8">
               <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
                 <span>Download CV</span>
                 <FiDownload className="text-xl"/>
               </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+              </div>
             </div>
           </div>
           {/*photo*/}
