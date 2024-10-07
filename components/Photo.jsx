c5257bd26d7a660787fd8cc9e662ec39f98981b2
henriquelbk <loubackhenrique123@gmail.com>
2024-10-06 23:49:33 -0300

    adjusting

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -7,9 +7,9 @@ const Photo = () => {
   return (
     <div className="w-full h-full relative">
         <motion.div initial={{ opacity: 0}} animate={{opacity: 1, transition: { delay: 2, duration: 0.4, ease: "easeIn" }}}>
-            <div className="w-[298px] h-[298px] xl:w-[398px] xl:h-[398px] mix-blend-lighten">
+            <motion.div initial={{ opacity: 1}} animate={{opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" }}} className="w-[298px] h-[298px] xl:w-[398px] xl:h-[398px] mix-blend-lighten">
                 <Image src="/assets/86364759.png" priority quality={100} fill alt="" className="object-contain"/>
-            </div>
+            </motion.div>
         </motion.div>
     </div>
   )
