3533d47f8fcb3b3d7a083c9240c53e7ca5a44adc
henriquelbk <loubackhenrique123@gmail.com>
2024-10-22 20:55:51 -0300

    Resume page v1

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,55 @@
+"use client";
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs } from "react-icons/fa"
+import { SiTailwindcss, SiNextdotja } from "react-icons/si";
+
+const about = {
+  title: "About me",
+  description: "fvafivauhfvasuvn",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Henrique Louback"
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(21) 09237107401"
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "2+ Years"
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Brazilian"
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "loubackhenrique123@gmail.com"
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available"
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Portuguese, Spanish"
+    }
+  ]
+}
+
+const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My Experience",
+  description: "vasdfovhasdfvnadfkv",
+  items: [
+    {
+      company: "",
+      position: "",
+      duration: "",
+    }
+  ]
+}
 
 
 const Resume = () => {
