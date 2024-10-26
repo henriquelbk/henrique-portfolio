43ff5facd63617887cfe95b51b52db737aea2136
Henrique Marques Louback <loubackhenrique123@gmail.com>
2024-10-25 23:45:12 -0300

    imports

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs } from "react-icons/fa"
-import { SiTailwindcss, SiNextdotja } from "react-icons/si";
+import { SiTailwindcss, SiNextdotjs, SiNextdotjs } from "react-icons/si";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
 
 const about = {
   title: "About me",
@@ -51,6 +55,73 @@ const experience = {
   ]
 }
 
+const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My Education",
+  description: "vasdfovhasdfvnadfkv",
+  items: [
+    {
+      institution: "Fluminense Federal University - Brazil",
+      degree: "Bachelor in Environmental Science",
+      duration: "2019 - 2023",
+    },
+    {
+      institution: "Labenu",
+      degree: "Fullstack Web Development Bootcamp",
+      duration: "2022 - 2023 - 1000 hours",
+    },
+    {
+      institution: "Online Course",
+      degree: "Web3 Development",
+      duration: "2024 - 2025 - X hours",
+    },
+    {
+      institution: "Yet to be confirmed - Canada",
+      degree: "Fullstack Web Development",
+      duration: "2025 - 2027",
+    }
+  ]
+}
+
+const skills = {
+  title: "My Skills",
+  description: "vasdfovhasdfvnadfkv",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "html 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javascript",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "node.js",
+    },
+    {
+      icon: <FaReact />,
+      name: "react.js",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "next.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwind.css",
+    },
+    {
+      icon: <FaFigma />,
+      name: "figma",
+    }
+    
+  ]
+}
 
 const Resume = () => {
   return (
