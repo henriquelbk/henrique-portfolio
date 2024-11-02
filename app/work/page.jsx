53a58b3219049f7c42b3319ae3d9435366043996
Henrique Marques Louback <loubackhenrique123@gmail.com>
2024-11-02 11:16:26 -0300

    workpage v2 changes

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -9,9 +9,71 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@compo
 import Link from "next/link";
 import Image from "next/image";
 
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "Project X",
+    description: "Project X was created for the frontend...",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assets/images/",
+    live: "",
+    github: ""
+  },
+  {
+    num: "02",
+    category: "backend",
+    title: "Project Y",
+    description: "Project Y was created for the backend...",
+    stack: [{ name: "Typescript" }, { name: "Node.js" }],
+    image: "/assets/images/",
+    live: "",
+    github: ""
+  },
+  {
+    num: "03",
+    category: "fullstack",
+    title: "Project T",
+    description: "Project T was created to be...",
+    stack: [{ name: "Next.js" }, { name: "Tailwind.css" }, { name: "Node.js" }], 
+    image: "/assets/images/",
+    live: "",
+    github: ""
+  },
+  {
+    num: "04",
+    category: "blockchain",
+    title: "Project Z",
+    description: "Project Z was created for the blockchain...",
+    stack: [{ name: "Solidity" }, { name: "Javascript" }], 
+    image: "/assets/images/",
+    live: "",
+    github: ""
+  },
+  {
+    num: "05",
+    category: "blockchain",
+    title: "Project W",
+    description: "Project W was created for the blockchain...",
+    stack: [{ name: "Solidity" }, { name: "Typescript" }, { name: "React" }],
+    image: "/assets/images/",
+    live: "",
+    github: ""
+  }
+]
+
 const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
   return (
-    <div>work page</div>
+    <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]"> 
+          <div className="w-full">text</div>
+          <div className="w-full">slider</div>
+        </div>
+      </div>
+    </motion.section>
   )
 }
 
